c714d9d6ea9468fcd9e851eb963935bf0e335dff
SunkavalliBhargavi <118039105+SunkavalliBhargavi@users.noreply.github.com>
2022-11-12 06:05:36 +0000

    Add files via upload

---
+++ Assignment1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegateEx
+{
+    public delegate void mydele(int a, int b);
+
+    class Dele
+    {
+        public void add(int a, int b)
+        {
+            Console.WriteLine("The sum is" + (a + b));
+        }
+        public void sub(int a,int b)
+        {
+            Console.WriteLine("The Difference is" + (a - b));
+            Console.ReadKey();
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Dele obj = new Dele();
+            mydele m = new mydele(obj.add);
+            m(20, 30);
+            mydele m1 = new mydele(obj.sub);
+            m1(20, 30);
+            Dele obj1 = new Dele();
+            mydele m2 = new mydele(obj1.add);
+            mydele m3 = new mydele(obj1.sub);
+            m2 = m2 + m3;
+            m2(30, 20);
+        }
+    }
+}
