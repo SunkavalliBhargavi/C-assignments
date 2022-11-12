98afdcd198767b020f312628a621acbab25112c8
SunkavalliBhargavi <118039105+SunkavalliBhargavi@users.noreply.github.com>
2022-11-12 06:18:07 +0000

    Add files via upload

---
+++ Assignment5/Class2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    internal class Class2
+    {
+        public class Assign2
+        {
+            public static void Main(string[] args)
+            {
+                EmpDetails emp = new EmpDetails();
+
+                ArrayList Emp_1 = new ArrayList();
+
+                Console.WriteLine("Please Enter The Employee 1 ID : ");
+                emp.Emp1_Id = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please Enter The Employee 1 Name : ");
+                emp.Emp1_Name = Console.ReadLine().ToString();
+
+                Console.WriteLine("Please Enter The Employee 1 Company : ");
+                emp.Emp1_Company = Console.ReadLine().ToString();
+
+                Emp_1.Add(emp.Emp1_Id);
+
+                Emp_1.Add(emp.Emp1_Name);
+
+                Emp_1.Add(emp.Emp1_Company);
+                Console.WriteLine("Employee 1 Array List Details");
+                foreach (var str in Emp_1)
+
+                {
+
+                    Console.WriteLine(str);
+
+                }
+                ArrayList Emp_2 = new ArrayList();
+
+                Console.WriteLine("Please Enter The Employee ID : ");
+                emp.Emp2_Id = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please Enter The Employee Name : ");
+                emp.Emp2_Name = Console.ReadLine().ToString();
+
+                Console.WriteLine("Please Enter The Employee Company : ");
+                emp.Emp2_Company = Console.ReadLine().ToString();
+
+                Emp_2.Add(emp.Emp2_Id);
+
+                Emp_2.Add(emp.Emp2_Name);
+
+                Emp_2.Add(emp.Emp2_Company);
+                Console.WriteLine("Employee 2 Array List Details");
+                foreach (var str in Emp_2)
+
+                {
+
+                    Console.WriteLine(str);
+
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Assignment5/Class4.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    internal class Class4
+    {
+        class Employee
+        {
+
+            // Declare 4 variables - id, name,
+            // department, and salary
+            int id;
+            int salary;
+            string name;
+            string department;
+
+            // Get the to string method that returns
+            // id, name, department, and salary
+            public override string ToString()
+            {
+                return id + " " + name + " " +
+                salary + " " + department;
+            }
+
+            // Driver code
+            static void Main(string[] args)
+            {
+
+                // Declare a list variable
+                List<Employee> emp = new List<Employee>()
+    {
+		
+		// Create 5 Employee details
+		new Employee{ id = 201, name = "Druva",
+                    salary = 12000, department = "HR" },
+        new Employee{ id = 202, name = "Deepu",
+                    salary = 15000, department = "Development" },
+        new Employee{ id = 203, name = "Manoja",
+                    salary = 13000, department = "HR" },
+        new Employee{ id = 204, name = "Sathwik",
+                    salary = 12000, department = "Designing" },
+        new Employee{ id = 205, name = "Suraj",
+                    salary = 15000, department = "Development" }
+    };
+
+                // Iterate the Employee by selecting Employee
+                // name starts with D
+                // Using IEnumerable interface
+                IEnumerable<Employee> result = emp.Where(x => x.name[0] == 'D');
+
+                // Display employee details
+                Console.WriteLine("ID Name Salary Department");
+                Console.WriteLine("++++++++++++++++++++++++++++");
+                foreach (Employee e in result)
+                {
+
+                    // Call the to string method
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+    }
+}
